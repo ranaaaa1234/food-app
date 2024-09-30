c094c8acfb8704ecd09088af30bc6d1bcd553157
rana safi <ranaa_safi@hotmail.com>
2024-09-30 12:46:53 +0200

    rjg

--- food-app/src/main.jsx
+++ food-app/src/main.jsx
@@ -1,3 +1,4 @@
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
@@ -6,6 +7,5 @@ import App from './App.jsx'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-
   </React.StrictMode>,
 )
