3ecb3608c391c0a7ae0c36757a228c540eb61402
rana safi <ranaa_safi@hotmail.com>
2024-10-03 09:37:39 +0200

    removed soicals from header

--- food-app/src/header/header.jsx
+++ food-app/src/header/header.jsx
@@ -13,15 +13,11 @@ function Header() {
 
       <div className='headerTitle'> 
         <h1>Food Explorer</h1>
-        <p className='intro'>Embark on a global flavor adventure with 
-          'Food Explorer'! Explore diverse recipes from around the world and 
-          indulge your taste buds. Start your delicious journey today!
-        </p>
       </div>
 
       <div className='headerContent'> 
 
-        <div className='socialsHeader'>
+        {/* <div className='socialsHeader'>
           <a href=''>
           <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="white" class="bi bi-facebook" viewBox="0 0 16 16">
             <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951"/>
@@ -37,7 +33,7 @@ function Header() {
             <path d="M12.6.75h2.454l-5.36 6.142L16 15.25h-4.937l-3.867-5.07-4.425 5.07H.316l5.733-6.57L0 .75h5.063l3.495 4.633L12.601.75Zm-.86 13.028h1.36L4.323 2.145H2.865z"/>
             </svg>
           </a>
-        </div>
+        </div> */}
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Rubik+Scribble&display=swap" />
       </div>
     </div>
